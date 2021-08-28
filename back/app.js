a7a41cb38ad9022ae9e1f30e02d8e99a51efa06f
SEOYUN KIM <nado0903@gmail.com>
2021-08-28 19:02:53 +0900

    feat: Add node

---
+++ back/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const postRouter = require('./routes/post');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello express');
+});
+app.get('/', (req, res) => {
+  res.send('hello api');
+});
+
+app.get('/posts', (req, res) => {
+  res.send([
+    { id: 1, content: 'hello' },
+    { id: 2, content: 'hello1' },
+    { id: 3, content: 'hello3' },
+  ]);
+});
+
+app.use('/post', postRouter); //중복되는 post prefix로 빼주기
+
+app.listen(3065, () => {
+  console.log('서버 실행 중');
+});
